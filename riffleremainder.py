c5eae916534f182fec370b200a10770705866bf3
Collin A Guo <bunjuku8@gmail.com>
2024-08-12 18:06:07 -0700

    added binomial cut

--- riffleremainder.py
+++ riffleremainder.py
@@ -1,10 +1,24 @@
 import random
-
+   
+from scipy.stats import binom   
 remaindertally = 0
+n = 52
+p = 0.5
+r_values = list(range(n + 1)) 
+dist = [binom.pmf(r, n, p) for r in r_values]
+
 
 for i in range(10000):
-    deck1 = 26
-    deck2 = 26
+    
+    dist_rand = random.uniform(0,1)
+    dist_marker = 0
+    deck1 = 0
+    for i, prob in enumerate(dist):
+        dist_marker += prob
+        if dist_rand < dist_marker:
+            deck1 = i
+            break
+    deck2 = 52-deck1
     while deck1 > 0 and deck2 > 0:
         randomdeck = random.randint(1,2)
         if randomdeck == 1:
